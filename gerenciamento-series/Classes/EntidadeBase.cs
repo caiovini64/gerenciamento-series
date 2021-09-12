bae8044943492e76d4d67058e5fea0e365c42e61
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-09-12 19:17:02 -0300

    cria métodos da classe Serie

--- gerenciamento-series/Classes/EntidadeBase.cs
+++ gerenciamento-series/Classes/EntidadeBase.cs
@@ -2,6 +2,6 @@ namespace gerenciamento_series
 {
     public abstract class EntidadeBase
     {
-        public int Id { get; protected set;}
+        protected int Id { get;  set;}
     }
 }
--- gerenciamento-series/Classes/Serie.cs
+++ gerenciamento-series/Classes/Serie.cs
@@ -1,13 +1,43 @@
 ﻿
 
+using System;
+
 namespace gerenciamento_series
 {
     class Serie : EntidadeBase
     {
         private Genero Genero { get; set; }
-        private string Titulo { get; set; }
+        private string Titulo {  get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+
+        public Serie(Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano + Environment.NewLine;
+            return retorno;
+        }
 
     }
 }
