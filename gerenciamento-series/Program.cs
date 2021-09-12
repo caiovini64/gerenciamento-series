1d2558c3d069d68079a060d1d911a50ecac956db
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2021-09-12 18:43:23 -0300

    Adicionar arquivos de projeto.

---
+++ gerenciamento-series/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gerenciamento_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
